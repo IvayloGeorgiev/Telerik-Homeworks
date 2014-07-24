601ad9a4823ab590b77b4037b9222b7024d421d7
IvayloGeorgiev <georgievir@yahoo.com>
2014-07-24 12:24:00 +0100

    JS 4 - Applications, Homework 2, Quick and Dirty version.

---
+++ C09_Javascript-Apps/H02_Web-Storage/scripts/gameLogic.js
@@ -0,0 +1,57 @@
+﻿var GameLogic = (function () {
+    var Logic = function Logic() {
+        this.number = [];
+    };
+
+    function GetRandomNumber(low, high) {
+        var number = Math.floor((Math.random() * high) + low);        
+        return number;
+    }
+
+    function checkRams(actual, guess) {
+        var rams = 0;
+        for (var i = 0, length = actual.length; i < length; i++) {
+            if (actual[i] === guess[i]) {
+                rams += 1;
+            }
+        }
+        return rams;
+    }
+
+    function checkSheep(actual, guess) {
+        var sheep = 0;
+        for (var i = 0, length = actual.length; i < length; i++) {
+            for (var j = 0; j < length; j++) {
+                if (i !== j && actual[i] === guess[j]) {
+                    sheep++;
+                }
+            }
+        }
+        return sheep;
+    }
+
+    Logic.prototype = {
+        generateNumber: function () {
+            var array = [],
+                numIndex,
+                availableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            while (array.length < 4) {
+                numIndex = GetRandomNumber(0, availableNumbers.length);
+                array.push(availableNumbers[numIndex]);
+                availableNumbers.splice(numIndex, 1);
+            }
+            this.number = array;
+        },
+
+        checkGuess: function (guess) {
+            var rams = checkRams(this.number, guess);
+            var sheep = checkSheep (this.number, guess);
+            return {
+                sheep: sheep,
+                rams: rams
+            };
+        }       
+    };
+
+    return Logic;
+})();
---
+++ C09_Javascript-Apps/H02_Web-Storage/scripts/gameStart.js
@@ -0,0 +1,102 @@
+﻿/// <reference path="libs/jquery-2.1.1.min.js" />
+/// <reference path="libs/underscore.js" />
+/// <reference path="gameLogic.js" />
+
+(function () {
+    var RAM_AND_SHEEP_GAME = 'RamAndSheepGame';
+    var highScores = loadHighScores();
+    var logic = new GameLogic();
+    var score = 100;        
+    logic.generateNumber();
+    console.log(logic.number);
+    $('.get-user-input').on('click', onGuessButtonClick);
+    $('.get-username').on('click', onSubmitUsernameClick);
+    displayHighScores();
+
+    function loadHighScores() {        
+        var scores = JSON.parse(localStorage.getItem(RAM_AND_SHEEP_GAME)) || [];                        
+        return scores;
+    }
+
+    function saveHighScores() {
+        for (var i = 0, length = highScores.length; i < length; i++) {
+            localStorage.setItem(RAM_AND_SHEEP_GAME, JSON.stringify(highScores));
+        }
+    }
+
+    function addScore(nickname) {
+        var result = {
+            nickname: nickname,
+            score: score
+        };        
+        highScores.push(result);        
+        _.sortBy(highScores, 'score').reverse();
+    }
+
+    function onGuessButtonClick() {
+        $this = $(this);        
+        var guessAsString = $('.input-field').val();        
+        if (validateInput(guessAsString)) {
+            var guess = [];
+            for (var i = 0, length = guessAsString.length; i < length; i++) {
+                guess.push(parseInt(guessAsString[i]));
+            }
+            var guessResult = logic.checkGuess(guess);
+            if (guessResult.rams === 4) {
+                $('.username-div').css('display', 'block');
+                $('.final-score').html('Final score: ' + score);
+                $('.user-input').css('display', 'none');
+            } else {                
+                var listContainer = $('.guesses-list');
+                listContainer.prepend(generateGuessContainer(guess, guessResult));
+                score -= 1;
+            }
+        }
+    }
+
+    function onSubmitUsernameClick(){
+        var nick = $('.username-field').val();        
+        if (nick.length > 3) {            
+            addScore(nick);
+            saveHighScores();
+            $('.username-div').css('display', 'none');
+            $('.game-over').css('display', 'block');
+        }        
+    }
+
+    function validateInput(string){
+        if (string.length !== 4){
+            return false;
+        } else if (isNaN(string)){
+            return false;
+        } else if (string[0] === '0'){
+            return false;
+        } 
+        for (var i = 0, length = string.length; i < length; i++) {           
+            if (isNaN(string[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function displayHighScores() {        
+        $('.high-scores-list').empty();        
+        highScores = _.sortBy(highScores, 'score').reverse();        
+        
+        for (var i = 0, length = highScores.length; i < length; i++){            
+            $('.high-scores-list').append(generateHighScoreContainer(highScores[i]));
+        }
+    }
+
+    function generateHighScoreContainer(item) {        
+        return $('<li>').html(item.nickname + ' - ' + item.score).addClass('score-item');
+    }
+
+    function generateGuessContainer(guess, guessResult) {        
+        var result = $('<li>').addClass('guess-item');
+        $('<div>').html(guess.join(',')).appendTo(result);
+        $('<div>').html('Rams - ' + guessResult.rams + '; Sheep - ' + guessResult.sheep).appendTo(result);
+        return result;
+    }
+})();
